d544fb7f48126946f23d7d6543116036dadd15df
rizqialfani01 <rizqialfani01@gmail.com>
2020-01-10 20:09:01 +0700

    Revert "Update tambah_penghuni.php"
    This reverts commit b82b40a2a2ad37b31bd5f31df9b00ff04c0c2ff8.

--- tambah_penghuni.php
+++ tambah_penghuni.php
@@ -183,6 +183,7 @@
                     <div class="col-md-6">
                         <div class="card">
                             <div class="card-body">
+<<<<<<< HEAD
                                 <form class="form-horizontal form-material" action="insert.php">
                                     <div class="d-flex m-b-30 align-items-center no-block">
                                         <h4 class="card-title ">Penghuni 1</h5>
@@ -192,6 +193,15 @@
                                                 <a class="btn btn-dark btn-circle fa fa-close" href="kamar.php"></a>
                                             </ul>
                                         </div>
+=======
+                                <div class="d-flex m-b-30 align-items-center no-block">
+                                    <h4 class="card-title ">Penghuni 1</h5>
+                                    <div class="ml-auto">
+                                        <ul class="list-inline font-12">
+                                            <a class="btn btn-dark btn-circle fa fa-check"></a>
+                                            <a class="btn btn-dark btn-circle fa fa-close"></a>
+                                        </ul>
+>>>>>>> parent of fec6d7a... Update tambah_penghuni.php
                                     </div>
                                 </div>
                                 <form class="form-horizontal form-material">
@@ -246,6 +256,7 @@
                                     <div class="form-group">
                                         <label class="col-md-4" style="float:left; height: 38px; padding: 10px">Agama</label>
                                         <div class="col-md-8" style="float:right;">
+<<<<<<< HEAD
                                             <select class="form-control form-control-line" name="agama">
                                                 <option disabled="disabled" selected>Pilih Agama</option>
                                                 <option value="Islam">Islam</option>
@@ -255,6 +266,9 @@
                                                 <option value="Biddha">Buddha</option>
                                                 <option value="Konghucu">Konghucu</option>
                                             </select>
+=======
+                                            <input type="text" placeholder="Agama Penghuni" class="form-control form-control-line">
+>>>>>>> parent of fec6d7a... Update tambah_penghuni.php
                                         </div>
                                     </div>
                                     <div class="form-group">
@@ -296,6 +310,7 @@
                                     <div class="form-group">
                                         <label class="col-md-4" style="float:left; height: 38px; padding: 10px">Tahun Masuk</label>
                                         <div class="col-md-8" style="float:right;">
+<<<<<<< HEAD
                                             <select class="form-control form-control-line" name="tahun_masuk">
                                                 <option disabled="disabled" selected>Pilih Tahun</option>
                                                 <option value="2020">2020</option>
@@ -310,11 +325,15 @@
                                                 <option value="2029">2029</option>
                                                 <option value="2030">2030</option>
                                             </select>
+=======
+                                            <input type="text" placeholder="Tahun Masuk Penghuni" class="form-control form-control-line">
+>>>>>>> parent of fec6d7a... Update tambah_penghuni.php
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <label class="col-md-4" style="float:left; height: 38px; padding: 10px">Masa Huni</label>
                                         <div class="col-md-8" style="float:right;">
+<<<<<<< HEAD
                                             <select class="form-control form-control-line" name="masa_huni">
                                                 <option disabled="disabled" selected>Pilih Masa Huni</option>
                                                 <option value="1">1 Tahun</option>
@@ -322,6 +341,9 @@
                                                 <option value="3">3 Tahun</option>
                                                 <option value="4">4 Tahun</option>
                                             </select>
+=======
+                                            <input type="text" placeholder="Masa Huni" class="form-control form-control-line">
+>>>>>>> parent of fec6d7a... Update tambah_penghuni.php
                                         </div>
                                     </div>
                                     <div class="form-group">
@@ -333,13 +355,21 @@
                                     <div class="form-group">
                                         <label class="col-md-4" style="float:left; height: 38px; padding: 10px">Bayar</label>
                                         <div class="col-md-8" style="float:right;">
+<<<<<<< HEAD
                                             <input type="text" placeholder="Jumlah Bayar Dimuka (Tanpa Titik)" class="form-control form-control-line" name="bayar" required>
+=======
+                                            <input type="text" placeholder="Jumlah Bayar Dimuka" class="form-control form-control-line">
+>>>>>>> parent of fec6d7a... Update tambah_penghuni.php
                                         </div>
                                     </div>
                                     <div class="form-group">
                                         <label class="col-md-4" style="float:left; height: 38px; padding: 10px">Piutang</label>
                                         <div class="col-md-8" style="float:right;">
+<<<<<<< HEAD
                                             <input type="text" placeholder="Jumlah Piutang (Tanpa Titik)" class="form-control form-control-line" name="piutang" required>
+=======
+                                            <input type="text" placeholder="Jumlah Piutang" class="form-control form-control-line">
+>>>>>>> parent of fec6d7a... Update tambah_penghuni.php
                                         </div>
                                     </div>
                                 </form>
